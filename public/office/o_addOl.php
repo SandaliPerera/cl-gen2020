d7b0b68eff77905c472422e0ae14fcd6bbac4e49
Dil-G <54798549+Dil-G@users.noreply.github.com>
2020-11-03 22:21:59 +0530

    Update o_addOl.php

--- public/office/o_addOl.php
+++ public/office/o_addOl.php
@@ -1,12 +1,5 @@
 
 
-<!DOCTYPE html>
-<html>
-<head><title>Add O/L Results</title>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link type="text/css" rel="stylesheet" href="../css/pop.css">
-<script src="../js/jquery-1.9.1.min.js"></script>
-<script src="../js/pop.js"></script>
 
 <!DOCTYPE html>
 <html>
