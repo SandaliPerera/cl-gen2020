b2b14f911e2a97ec050cd92477e1ebf614a0e1c0
SandaliPerera <hazsanu@gmail.com>
2020-11-03 21:05:02 +0530

    Create o_alCommerce.php

---
+++ public/office/o_alCommerce.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>G.C.E. A/L Examination Results - 2016</title>
+    <link rel="stylesheet" href="../css/view.css " type="text/css">
+    <link type="text/css" rel="stylesheet" href="../css/main.css">
+    <link type="text/css" rel="stylesheet" href="../css/users.css">
+  </head>
+  <body>
+    
+  <div id="nav"></div>
+         <div class="content">
+		
+            <h1>G.C.E. A/L Examination  Results - 2016</h1>
+            <form class="search" action="o_addStudentDetails.html">
+            <input type="text" placeholder="Search.." name="search">
+            <button type="submit">Search</button>
+            </form>
+            
+            
+            <br>
+            <br>
+            <br>
+             
+                
+                  <div class="card">
+            
+                    <br>
+                    
+                    <hr>
+
+                    <table>
+                        <thead><i><h3>Commerce Stream</h3></i></thead>
+                        <hr>
+                        <tr>
+                          <th rowspan = "2" >Student Index Number</th>
+                          <th rowspan = "2" >A/L Index Number</th>
+                          <th rowspan="2">Student Name</th>
+                          <th colspan = "6" >Subjects</th>
+                        </tr>
+                        
+                        <tr>
+                          <th>Business Studies</th>
+                          <th>Accounting and Finance</th>
+                          <th>Economics</th>
+                          <th>ICT</th>
+                          <th>General English</th>
+                          <th>General Knowledge</th>
+                        </tr>
+                        <tr>
+                          <td>S12345</td>
+                          <td>65610</td>
+                          <td>A.B.C. Student</td>
+                          <td>A</td>
+                          <td>C</td>
+                          <td>B</td>
+                          <td>A</td>
+                          <td>A</td>
+                          <td>86</td>
+                        </tr>
+                          
+                        
+                      </table>
+                    
+                    </div>
+            
+				
+		</div>
+		
+</body>
+</html>
+
+
+
