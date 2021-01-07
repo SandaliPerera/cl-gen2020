735f8405c6927084d36231c09663a864abf9f15b
Sandali Hasara Perera <56555623+SandaliPerera@users.noreply.github.com>
2021-01-07 23:59:12 +0530

    Update o_scholCsv.php

--- public/office/o_scholCsv.php
+++ public/office/o_scholCsv.php
@@ -27,21 +27,41 @@
 
 <body>
     <div id="officeNav"></div>
+    <?php
+
+        require_once '../../config/conn.php';
+
+        $sql = "SELECT * FROM addscholexam where examID='".$_GET['examID']."'";
+
+        $res= mysqli_query($conn,$sql);
+        $row=mysqli_fetch_array($res);
+
+        $examID = $row['examID'];
+        $charID = substr($examID,0);
+       // $pID = "PR" . $charID;
+
+        if($res){
+        //echo "Sucessfull";
+        }
+        else{
+        echo"failed";	
+        }
+    ?>
 
     <div class="content">
         <div class="container" style="margin-left:250px;">
-            <form action="../php/register.php" method="POST">
+            <form action="../../src/o_scholCsv.php" method="POST">
                 <h1>Add Grade 5 Scholarship Exam Results</h1>
                 <hr>
 
                 <label for="examID"><b>Exam ID</b></label>
-                <input type="text" value="G5S123" name="examID" readonly>
+                <input type="text" value= "<?php if (isset ($_GET['examID'])){echo $_GET['examID'];}?>" name="examID" readonly>
 
                 <label for="scholExamYear"><b>Enter Exam Year</b></label>
-                <input type="text" value="2016" name="scholExamYear" readonly>
+                <input type="text" name="scholExamYear" value= "<?php if (isset ($_GET['scholExamYear'])){echo $_GET['scholExamYear'];}?>" name="scholExamYear" readonly>
 
                 <label for="examName"><b>Exam Name</b></label>
-                <input type="text" value="Grade 5 Scholarship Exam - 2016" name="examName" readonly>
+                <input type="text" name="examName" value= "<?php if (isset ($_GET['examName'])){echo $_GET['scholExamYear'];}?>" name="ExamName" readonly>
 
                 <label for="myFile"><b>Enter CSV File</b></label>
                 <input type="file" id="myFile" name="filename" class="nextpgbtn" required></br>
